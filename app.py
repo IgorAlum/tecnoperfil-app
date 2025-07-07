845b0386160514fd490253ee87c2ce8291d63a26
IgorAlum <igor@tecnoperfilaluminio.com.br>
2025-07-07 16:04:34 -0300

    Rename app.py to streamlit_app.py

--- app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.title("Tecnoperfil - Proposta Automática")
-st.write("Este é o app de geração automática de propostas técnicas e comerciais.")
---
+++ streamlit_app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("Tecnoperfil - Proposta Automática")
+st.write("Este é o app de geração automática de propostas técnicas e comerciais.")
